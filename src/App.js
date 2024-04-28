be2f80a6e5c866f37d928f77083090bc6ad255c6
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-04-28 21:02:43 +0530

    Default export

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
-import { backdropClasses, buttonGroupClasses, imageListClasses } from "@mui/material";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import FoodFireLogo from "../Images/Food Fire Logo.png";
-
-const Title=()=>(
-        <a href="/">
-        <img
-        className="logo"
-        alt="Image" 
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVhCDV1Xj1jePbITftwhcH4-nBlX1aYx8DE0kw6zOE3bya4iTwv6AHN1111dml-VtufYw&usqp=CAU" >
-                {/* Alt must be mention before src  */}
-        </img>
-        </a>
-
-
-);
+import Title from './components/Title'
 const Header = ()=>{
         return(
                 <div className="header">
---
+++ src/components/Title.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title=()=>(
+    <a href="/">
+    <img
+    className="logo"
+    alt="Image" 
+    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRVhCDV1Xj1jePbITftwhcH4-nBlX1aYx8DE0kw6zOE3bya4iTwv6AHN1111dml-VtufYw&usqp=CAU" >
+            {/* Alt must be mention before src  */}
+    </img>
+    </a>
+
+
+
+);
+
+export default Title;
