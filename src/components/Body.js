2dd9d9bbb790acdd62322382216ae763dc0dd3a6
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-04-29 22:42:37 +0530

    Toggle using onCLick

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,48 +3,29 @@ import RestaurantCard from "./RestaurantCard"
 import { useState } from "react";
 const Body =()=>{
 
-  let searchTxt = "KFC";
+  const [searchText,setSearchText]= useState("false");
 
-  const [searchText,setSearchText]= useState("KFC");//to create state variables  , it returns a array and the first el of arr is variale name here KFC is default value 
-  // A Hook is just a fun 
-
-  //searchText-> variabe name
-  // setSearchText->method to update it 
-
-  // searchTxt=e.target.value;  //can not modify vaibles using usestate like this 
-
-
-   
-    return   (    
+  return (
     <>
       <div className="search-container">
         <input type="text"
          className="search-input"
          placeholder="Search"
-        //  value={searchTxt}  //useless
-        value={searchText}
+         value={searchText}
          onChange={(e)=>{
-          // e-> Event 
-          console.log("On CHnaging stg on search I will be printed")
-          console.log(e.target.value)
-          // On writing stg on input KFC+input will be printed 
-
-          searchTxt=e.target.value;  //can not modify vaibles using usestate like this 
-
-          // It also wont work bs local variable (searchTxt) is not preferref in react make a react variable 
-          /*{
-            Every component in react maintains a state and u variables into a state and every time u create a local varibale u use useState hook in react  
-          }*/
-
-          setSearchText(e.target.value)   //Modified successfully 
-          // e.target.value => whatever  u write in input
+          setSearchText(e.target.value)  
           }
-
-          
         }
-         />
-         <button className="search-btn">Search-{searchText}</button>
-      </div>      
+        />
+        <h1>{searchText}</h1>
+        <button 
+        className="search-btn"
+        onClick={()=>{
+          if(searchText==="true") setSearchText("false");
+          else setSearchText("true")
+        }}
+        >Search</button>
+        </div>        
     <div className="restaurant-list">
           {
             restaurantList.map((restaurant) =>{
@@ -59,6 +40,10 @@ const Body =()=>{
     
 }
 export default Body
+/*
+Que :- If we had local variables why we need this state variable 
 
-// [searchText,setSearchText]-> 
-// every time we set it gets updates so 2 way binding 
+If someone updated my local variable I wont know React has no idea of this 
+
+in   const [searchText,setSearchText]= useState("KFC"); //react is watching these varibales  
+*/
