bec5acbf28fdcb30b06c6374c4792eb6922f760b
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-01 19:46:11 +0530

    fetched restaurants data

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,7 +1,25 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom"
+
 const RestaurantMenu =()=>{
+    //to read a dynamic URL params 
+    const params =useParams();
+
+    useEffect( ()=>{
+        getRestaurantInfo();
+    },[]);
+    async function getRestaurantInfo(){
+        const data =await fetch("https://foodfire.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=21.1702401&lng=72.83106070000001&&submitAction=ENTER&restaurantId="
+    );
+
+    const json = await data.json();
+    console.log(json);
+}
+
+    const {id}=params;
     return (
         <div>
-            <h1>Restaurant id:123</h1>
+            <h1>Restaurant id: {id}</h1>
             <h2>namaste</h2>
         </div>
     )
