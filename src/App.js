04cf31d7402d56cd6d96ddafdba94c6b7dc3f5ab
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-01 18:12:56 +0530

    Added RestaurantMenu

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { createBrowserRouter ,RouterProvider,Outlet} from "react-router-dom";
 import About from './components/About'
 import Error from './components/Error'
 import Contact from "./components/Contact";
+import RestaurantMenu from "./components/RestaurantMenu";
 
 const AppLayout = () => {
 
@@ -34,6 +35,9 @@ const appRouter = createBrowserRouter([
       },{
         path:"/contact",
         element:<Contact/>
+      },{
+        path:'/restaurant/:id',  //Dynamic routing 
+        element:<RestaurantMenu/>
       }
     ]
   }
---
+++ src/components/RestaurantMenu.js
@@ -0,0 +1,9 @@
+const RestaurantMenu =()=>{
+    return (
+        <div>
+            <h1>Restaurant id:123</h1>
+            <h2>namaste</h2>
+        </div>
+    )
+}
+export default RestaurantMenu
