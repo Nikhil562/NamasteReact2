a3901a3d22bd6b5aa06ff80b39a4e2db6f6e6589
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-02 17:08:52 +0530

    Added multiple state variables

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,16 @@
 import { useState } from "react"
 const Profile =(props)=>{
-    const [count]= useState(0);
+    const [count,setCount]= useState(0);
+    const [count2,setCount2]=useState(0);
     return (
         <div>
             <h1>Profile Component :{props.name}</h1>
             <h2>Count:- {count}</h2>
+            <h3>Count2:-{count2}</h3>
+            <button onClick={()=>{
+                setCount(1),
+                setCount2(2);
+            }}>count</button>
         </div>
     )
 }
--- src/components/profileClass.js
+++ src/components/profileClass.js
@@ -4,8 +4,9 @@ class Profile extends React.Component{
     constructor(props){
         super(props);
         this.state={
-            count:0
-        }
+            count:0,
+            count2:0
+        }//hole state is a 1 big obj react uses 1 big obj to maintain the hole state 
     }
     render(){
     return (
@@ -13,6 +14,13 @@ class Profile extends React.Component{
         <h1>Name:-{this.props.name}</h1>
         <h2>XYZ:- {this.props.xyz}</h2>
         <h3>count:-{this.state.count}</h3>
+        <h3>Count:-{this.state.count2}</h3>
+        <button onClick={()=>{
+            this.setState({
+                count:1,
+                count2:2
+            })
+        }}>SetCount</button>
         </div>
     )
 }
