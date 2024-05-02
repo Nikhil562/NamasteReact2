8feba0c677834763eb5ecc6ef68258f71077097f
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-02 10:20:46 +0530

    Created first class based component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,13 @@
 import {Outlet} from "react-router-dom"
+import Profile from "./profileClass"
 const About=()=> {
   return (
-    <div>About
-    <Outlet/>
-    {/* Or we could do <Profile/> */}
+    <div>
+      <h1>About Us Page</h1>
+      <p>
+        This is the Namaste react course
+      </p>
+      <Profile/>
     </div>
   )
 }
---
+++ src/components/profileClass.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Profile extends React.Component{
+    // u can not create a class based component without render method which returns some JSX 
+    render(){
+        return 
+        (
+        <h1>Profile Class Component </h1>
+        )
+    }
+}
+
+export default Profile;
