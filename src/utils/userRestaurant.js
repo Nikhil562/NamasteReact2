01a05fbf1d740eeb8141fd6b162ee51a14709b4d
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-03 18:07:22 +0530

    Exported FETCH_MENU_URL to constants

--- src/utils/userRestaurant.js
+++ src/utils/userRestaurant.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import {FETCH_MENU_URL} from '../constants'
 
 const useRestaurant =(resId)=>{
 const [restaurant,setRestaurant]= useState(null);
@@ -9,11 +9,9 @@ useEffect( ()=>{
     getRestaurantInfo();
 },[]);
 async function getRestaurantInfo(){
-    const data =await fetch("https://www.swiggy.com/dapi/menu/v4/full?lat=12.9351929&lng=77.62448069999999&menuId="+resId)
-
-const json = await data.json();
-console.log(json);
-setRestaurant(json.data);  //will be updated whenever data is fetched from API 
+    const data =await fetch(FETCH_MENU_URL+resId)
+    const json = await data.json();
+    setRestaurant(json.data); 
 }
 
 return restaurant;
