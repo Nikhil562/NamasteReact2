fe7510ec197c5000e9d4d8486930fc1284d51eec
Nikhil562 <nikhilsharmacfc@gmail.com>
2024-05-03 21:54:29 +0530

    removed eventlisteners

--- src/utils/useOnline.js
+++ src/utils/useOnline.js
@@ -4,16 +4,28 @@ const useOnline =()=>{
     const [isOnline,setIsOnline] =useState(true);
 
     useEffect(()=>{
-        window.addEventListener('online',()=>{
-            //whenever u go online this code will trigger
+
+        const handleOnLine=()=>{
             setIsOnline(true)
-        })
-        window.addEventListener('offline',()=>{
-            //whenever u go online this code will trigger
+        }
+
+        const handleOffLine=()=>{
             setIsOnline(false);
-        })
+        }
+        window.addEventListener('online',handleOnLine)
+        window.addEventListener('offline',handleOffLine)
+
+        return ()=>{
+            window.removeEventListener('online',handleOnLine)
+            window.removeEventListener('online',handleOffLine)
+        }
     },[])
 
     return isOnline;
 }
 export default useOnline
+
+
+///whenerver we are going online , coming online after offline a new event listener is not created bs we hv a empty dependency array [] i useeffect above  , it wont call this useEffect again 
+
+// But if We are on Home page and we move to ABout page the event listener is still there so its a goof practice to clr the event listeners whenever we go out of component 
